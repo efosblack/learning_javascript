30827e40d2eecb02a9ec918856926b2107a543a6
efosblack <eriamiatoeefe11@gmail.com>
2022-07-22 11:16:18 -0400

    null and undefined

--- CHAPTER_1/sanbox.js
+++ CHAPTER_1/sanbox.js
@@ -1,23 +1,4 @@
-//arrays
-let ninjas = ['efe', 'ryu', 'lu-kang'];
+let age;
+age = null;
 
-// ninjas[1] = 'jet-li';
-// console.log(ninjas);
-
-// let ages = [20, 25, 30, 35];
-// console.log(ages[2]);
-
-// let random = ['efe', 'crystal', 30,20];
-// console.log(random);
-
-//console.log(ninjas.length);
-
-// array methods
-
-//let result = ninjas.join(',');
-//let result = ninjas.indexOf('lu-kang');
-//let result = ninjas.concat(['ken', 'crystal']);
-let result = ninjas.push('ken'); //destructive method
-result = ninjas.pop(); //destructive method
-
-console.log(result);
+console.log(age, age + 3, `the age is ${age}`);
