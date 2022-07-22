75daeb6e47678eb582009c6c7bfe5be46dcd4f02
efosblack <eriamiatoeefe11@gmail.com>
2022-07-22 10:20:17 -0400

    numbers

--- CHAPTER_1/sanbox.js
+++ CHAPTER_1/sanbox.js
@@ -1,15 +1,32 @@
-// common string methods
+let radius = 10;
+const pi = 3.14;
 
-let email = 'test@company.com';
+// console.log(radius, pi);
 
-// let result = email.lastIndexOf('n');
+//math operators +, -, *, /, **(raisedToPower), %
 
-// let result = email.slice(0,5);
+// console.log(10/2);
+// let result = radius % 3;
+//let result = pi * radius**2;
 
-// let result = email.substr(4,7);
+//order of operation - B I(indices) D M A S
 
-// let result = email.replace('m', 'w');
+//let result = 5 * (10-3)**2;
 
+let likes = 10;
 
+//likes++;
+//likes--;
+//likes+=10;
+//likes-=5;
+//likes*=2;
+//likes/=2;
 
+//NaN - not a number
+
+// console.log(5/'hello');
+// console.log(5 * 'hello');
+
+//concatenating numbers and strings
+let result = 'the blog has ' + likes + ' likes';
 console.log(result);
