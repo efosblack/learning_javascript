5868b40f6f2320ecaf398d49b83bdb7611e7a789
efosblack <eriamiatoeefe11@gmail.com>
2022-07-22 10:51:59 -0400

    template strings

--- CHAPTER_1/sanbox.js
+++ CHAPTER_1/sanbox.js
@@ -1,32 +1,21 @@
-let radius = 10;
-const pi = 3.14;
+//template strings
+const title = 'Best reads of 2022';
+const author = 'Efosblack';
+const likes = 30;
 
-// console.log(radius, pi);
+//concatenation way
+// let result = 'The blog called ' + title + ' by ' + author + ' has ' + likes + ' likes';
+// console.log(result);
 
-//math operators +, -, *, /, **(raisedToPower), %
-
-// console.log(10/2);
-// let result = radius % 3;
-//let result = pi * radius**2;
-
-//order of operation - B I(indices) D M A S
-
-//let result = 5 * (10-3)**2;
-
-let likes = 10;
-
-//likes++;
-//likes--;
-//likes+=10;
-//likes-=5;
-//likes*=2;
-//likes/=2;
-
-//NaN - not a number
-
-// console.log(5/'hello');
-// console.log(5 * 'hello');
-
-//concatenating numbers and strings
-let result = 'the blog has ' + likes + ' likes';
+//template string way
+let result = `the blog called ${title} by ${author} has ${likes} likes`;
 console.log(result);
+
+//creating html templstes
+let html = `
+ <h2>${title}</h2>
+ <p>By ${author}</p>
+ <span>This blog has ${likes} likes</span>
+`;
+
+console.log(html);
