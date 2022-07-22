566794f988ee7344cb726c6313cfe63b17dc938c
efosblack <eriamiatoeefe11@gmail.com>
2022-07-22 11:37:53 -0400

    booleans and comparisons

--- CHAPTER_1/sanbox.js
+++ CHAPTER_1/sanbox.js
@@ -1,4 +1,31 @@
-let age;
-age = null;
+//booleans & comparisons
+console.log(true, false, 'true', 'false');
 
-console.log(age, age + 3, `the age is ${age}`);
+// methods can return booleans
+let email = 'blaq@company.com';
+let names =['mario', 'luigi', 'lee'];
+
+//let result = email.includes('@');
+//result = email.includes('!');
+// let result = names.includes('luigi');
+
+// console.log(result);
+
+//comparison operators
+let age = 25;
+
+// console.log(age == 25);
+// console.log(age == 30);
+// console.log(age != 30);
+// console.log(age > 20);
+// console.log(age < 20);
+// console.log(age <= 25);
+// console.log(age >= 25);
+
+let name = 'blaq';
+
+console.log(name == 'blaq');
+console.log(name == 'Blaq');
+console.log(name > 'crystal');
+console.log(name > 'Blaq');
+console.log(name > 'Crystal');
