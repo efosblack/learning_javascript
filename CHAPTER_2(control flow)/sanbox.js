1afadeb926e6a37a3bce3c3824253af73486cae8
efosblack <eriamiatoeefe11@gmail.com>
2022-07-22 14:09:50 -0400

    if statements

--- CHAPTER_2(control flow)/sanbox.js
+++ CHAPTER_2(control flow)/sanbox.js
@@ -1,9 +1,20 @@
-//do while loops
+//if statements
 
-let i = 5;
-do{
-    console.log('val of i is: ', i);
-    i++;
+// const age = 200;
 
- }while(i < 5);
+// if(age > 20){
+//     console.log('you are over 20 years old');
+// }
 
+// const ninjas = ['black', 'ryu', 'chun-li', 'yoshi'];
+
+// if(ninjas.length > 3){
+//     console.log("that's a lot of ninjas")
+// }
+
+
+const password = 'pass';
+
+if(password.length >= 8){
+    console.log('that password is long enough!');
+}
