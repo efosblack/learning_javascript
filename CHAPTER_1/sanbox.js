7ef645ad33bde0054f567fde9c2ed0a1621f98a9
efosblack <eriamiatoeefe11@gmail.com>
2022-07-22 11:50:31 -0400

    loose and strict comparisons

--- CHAPTER_1/sanbox.js
+++ CHAPTER_1/sanbox.js
@@ -1,31 +1,15 @@
-//booleans & comparisons
-console.log(true, false, 'true', 'false');
-
-// methods can return booleans
-let email = 'blaq@company.com';
-let names =['mario', 'luigi', 'lee'];
-
-//let result = email.includes('@');
-//result = email.includes('!');
-// let result = names.includes('luigi');
-
-// console.log(result);
-
-//comparison operators
 let age = 25;
 
+// loose comparison (different types can still be equal)
+
 // console.log(age == 25);
-// console.log(age == 30);
-// console.log(age != 30);
-// console.log(age > 20);
-// console.log(age < 20);
-// console.log(age <= 25);
-// console.log(age >= 25);
+// console.log(age == '25'); (type conversion makes the string a number)
+// console.log(age != 25);
+// console.log(age != '25');
 
-let name = 'blaq';
+// strict comparison (different types cannot be equal)
 
-console.log(name == 'blaq');
-console.log(name == 'Blaq');
-console.log(name > 'crystal');
-console.log(name > 'Blaq');
-console.log(name > 'Crystal');
+console.log(age === 25);
+console.log(age === '25'); //(no type conversion)
+console.log(age !== 25);
+console.log(age !== '25');
