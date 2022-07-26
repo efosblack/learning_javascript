afd7d222347fa38e40b6935359c3989a23b2424f
efosblack <eriamiatoeefe11@gmail.com>
2022-07-26 04:28:09 -0400

    logical NOT (!) operator

--- CHAPTER_2(control flow)/sanbox.js
+++ CHAPTER_2(control flow)/sanbox.js
@@ -1,11 +1,11 @@
-// logical operators - OR || and AND &&
+// logical NOT (!)
 
-const password = 'p@ss';
 
-if (password.length >= 12 && password.includes('@')){
-    console.log('that password is mighty strong!');
-} else if(password.length >= 8 || password.includes('@') && password.length >= 5){
-    console.log('that password is strong enough!');
-} else{
-    console.log('password is not long enough!');
+let user = false;
+
+if(!user){
+    console.log('you must be logged in to continue');
 }
+
+console.log(!true);
+console.log(!false);
