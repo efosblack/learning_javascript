dbd867e1f6418b32e5c795eba7ca2ba36df3e76b
efosblack <eriamiatoeefe11@gmail.com>
2022-07-26 09:02:13 -0400

    arrow functions

--- CHAPTER_3(functions)/sanbox.js
+++ CHAPTER_3(functions)/sanbox.js
@@ -1,15 +1,27 @@
-//returning values
 
-// const speak = function(name = 'human', time = 'day'){
-//     console.log(`good ${time} ${name}`);
+//regular function
+// const calcArea = function(radius){
+//     return  3.14 * radius**2;
+    
 // };
 
+// arrow function
+const calcArea = radius => 3.14 * radius**2;
 
-const calcArea = function(radius){
-    return  3.14 * radius**2;
-    
-};
 
 const area = calcArea(5);
-console.log(area);
+console.log('area is : ', area);
 
+
+const greet = () => 'hello, world';
+
+console.log(greet());
+
+const bill = (products, tax) => {
+    let total = 0;
+    for(let i = 0; i < products.length; i++){
+        total += products[i] + products[i] * tax;
+    }
+    return total;
+};
+console.log(bill([10, 15, 30], 0.2));
