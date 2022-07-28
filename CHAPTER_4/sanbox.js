061d4bef76469740ac224d5f4f8d9a6a50379819
efosblack <eriamiatoeefe11@gmail.com>
2022-07-28 04:59:11 -0400

    Math

--- CHAPTER_4/sanbox.js
+++ CHAPTER_4/sanbox.js
@@ -1,35 +1,17 @@
-// object methods
+// Math 
 
-// const blogs = [ 
-//     { title: 'why mac and cheese rules', likes: 30},
-//     { title: '10 things i love about berlin', likes: 20}
-// ];
+console.log(Math);
+console.log(Math.PI);
 
+const area = 7.1;
 
-let user = {
-    name: 'crystal',
-    age: 30,
-    email: 'crystal@company.com',
-    location: 'south pole',
-    blogs: [{ title: 'why mac and cheese rules', likes: 30},
-            { title: '10 things i love about berlin', likes: 20}],
-    login: function(){
-        console.log('the user logged in');
-    },
-    logout: () => console.log('the user logged out'),
+console.log(Math.round(area));
+console.log(Math.floor(area));
+console.log(Math.ceil(area));
+console.log(Math.trunc(area));
 
+//random numbers
 
-
-    logBlogs:function(){
-        console.log('this user has written the following blogs:');
-        this.blogs.forEach(blog =>{
-            console.log(blog.title, blog.likes);
-        })
-    }
-};
-
-// user.logout();
-user.logBlogs();
-// console.log(this);
-
-
+const random = Math.random();
+console.log(random);
+console.log(Math.round(random * 100));
