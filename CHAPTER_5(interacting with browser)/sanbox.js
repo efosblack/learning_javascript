c89cdd1e608fdccc2d5fa26874965b0322c7bfba
efosblack <eriamiatoeefe11@gmail.com>
2022-08-01 04:29:50 -0400

    getting elements from the DOM contd'

--- CHAPTER_5(interacting with browser)/sanbox.js
+++ CHAPTER_5(interacting with browser)/sanbox.js
@@ -1,14 +1,17 @@
-// const para = document.querySelector('div.error');
+// get an element by ID
+// const title = document.getElementById('page-title');
+// console.log(title);
 
-// console.log(para);
 
-const paras = document.querySelectorAll('p');
 
-// paras.forEach(para => {
-//     console.log(para);
-// });
-//console.log(paras);
+// get elements by their class name
+// const errors =document.getElementsByClassName('error');
+// console.log(errors);
+// console.log(errors[0]);
 
-const errors = document.querySelectorAll('.error');
 
-console.log(errors);
+
+// get elements by their tag name
+const paras = document.getElementsByTagName('p');
+console.log(paras);
+console.log(paras[1]);
