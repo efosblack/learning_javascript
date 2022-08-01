4b727ef50c108283824bfbaac69b7051c7de2d56
efosblack <eriamiatoeefe11@gmail.com>
2022-08-01 03:42:50 -0400

    using querrysellector

---
+++ CHAPTER_5(interacting with browser)/sanbox.js
@@ -0,0 +1,14 @@
+// const para = document.querySelector('div.error');
+
+// console.log(para);
+
+const paras = document.querySelectorAll('p');
+
+// paras.forEach(para => {
+//     console.log(para);
+// });
+//console.log(paras);
+
+const errors = document.querySelectorAll('.error');
+
+console.log(errors);
