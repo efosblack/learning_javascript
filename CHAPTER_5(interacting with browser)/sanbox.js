3c21df674222968b29a3cf76d832365312cd899f
efosblack <eriamiatoeefe11@gmail.com>
2022-08-02 06:06:36 -0400

    managing classes contd...

--- CHAPTER_5(interacting with browser)/sanbox.js
+++ CHAPTER_5(interacting with browser)/sanbox.js
@@ -7,3 +7,18 @@
 // content.classList.remove('error');
 // content.classList.add('success');
 
+const paras = document.querySelectorAll('p');
+
+paras.forEach(p => {
+    if(p.textContent.includes('error')){
+        p.classList.add('error');
+    }
+    if(p.textContent.includes('success')){
+        p.classList.add('success');
+    }
+});
+
+//toggle class
+const title = document.querySelector('.title');
+title.classList.toggle('test');
+title.classList.toggle('test');
